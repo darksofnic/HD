18c9768d2e5ef55f6b6e3f3b8d197fb59f245539
DarkSofnic <darksofnic@gmail.com>
2023-03-14 23:08:08 -0400

    Website

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,47 +1,36 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home/Home'
+import Admin from './components/Admin/Admin'
+import ExamDetails from './components/ExamDetails'
+import RecordList from './components/ExamList'
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
+import '../src/App.css'
+import useGetData from "./components/util/util";
+import { Navigate } from 'react-router-dom'
 
-// Constants
-import { routes } from './constants';
+function App() {
 
-// Styles
-import { CssBaseline } from '@material-ui/core';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
+  const data = useGetData();
 
-// Static/Stateless
-import { NavBar, Welcome } from './components';
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route index element={<Home data={data}/>}/>
+          <Route path='/admin' element={<Admin data={data}/>}/>
+          <Route path='/details/:patientId' element={<ExamDetails/>}/>
+          <Route exact path="/" element={<RecordList />} />     
+          <Route path="/oct-int-repo" element={<Navigate to="/" />} />  
+        </Routes>
+      </div>
+    </BrowserRouter>
 
-// Pages
-import { ItemInsert, Items, ItemUpdate } from './pages';
-
-class App extends Component {
-  render() {
-    // TODO: maybe only need one route for Items?
-    const publicViews = (
-      <Switch>
-        <Route exact path={routes.HOME}>
-          <Redirect to={routes.ITEMS} />
-        </Route>
-        <Route exact path={routes.ITEM_UPDATE} component={ItemUpdate} />
-        <Route exact path={routes.HOME} component={Welcome} />
-        <Route exact path={routes.ITEMS} component={Items} />
-        <Route exact path={`${routes.ITEMS}/items-plain`} component={Items} />
-        <Route exact path={`${routes.ITEMS}/react-table-v6`} component={Items} />
-        <Route exact path={routes.ITEM_INSERT} component={ItemInsert} />
-      </Switch>
-    );
-
-    return (
-      <BrowserRouter>
-        <CssBaseline />
-        <NavBar />
-        <div className="app--main">
-          <div className="view-container">{publicViews}</div>
-        </div>
-      </BrowserRouter>
-    );
-  }
+  )
 }
+
+
 
 export default App;
